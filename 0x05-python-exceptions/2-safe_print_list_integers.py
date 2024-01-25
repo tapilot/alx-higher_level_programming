242fc91156a0dc2e3b84e3e435bb595421fe6516
tapilot <abovuschools@gmail.com>
2024-01-25 11:47:56 -0800

    Add changes to function that prints first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=None, x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
-
-    try:
-        for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end=" ")
-                count += 1
-
-    except (IndexError, ValueError, TypeError):
-        pass
-
-    finally:
-        print()
-        return count
-
-
-if __name__ == "__main__":
-    my_list = [1, "two", 3, "four", 5]
-    x = 4
-    integers_printed = safe_print_list_integers(my_list, x)
-    print(f"Number of integers printed: {integers_printed}")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
