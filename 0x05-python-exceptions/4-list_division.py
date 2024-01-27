984f3b600c222e4f2f4ef5f06fe295153816b530
tapilot <abovuschools@gmail.com>
2024-01-27 03:33:49 -0800

    Added a function that divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,16 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            res = my_list_1[i] / my_list_2[i]
+            division = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("Wrong type")
-            res = 0
+            division = 0
         except ZeroDivisionError:
             print("division by 0")
-            res = 0
+            division = 0
         except IndexError:
             print("out of range")
-            res = 0
+            division = 0
         finally:
-            result.append(res)
-    return result
+            result.append(division)
+            return result
--- 0x05-python-exceptions/4-main.py
+++ 0x05-python-exceptions/4-main.py
@@ -3,12 +3,12 @@ list_division = __import__('4-list_division').list_division
 
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+result = list_division(my_l_1, my_l_2, 3)
 print(result)
 
 print("--")
 
 my_l_1 = [10, 8, 4, 4]
 my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+result = list_division(my_l_1, my_l_2, 5)
 print(result)
